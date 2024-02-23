a424a3ef7ec0bc80e2623ce77f40734c030e5ce9
Aditya Bhattad <adityabhattad18@gmail.com>
2024-02-24 00:13:12 +0530

    question answer service working

--- question_answer/src/app.py
+++ question_answer/src/app.py
@@ -1,8 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Path as PathParam, Query, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 import os
 from dotenv import find_dotenv, load_dotenv
-from pydantic import BaseModel
+from pydantic import BaseModel,Field
 from langchain.prompts import PromptTemplate
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_google_genai import ChatGoogleGenerativeAI
@@ -11,13 +11,14 @@ from langchain.chains import LLMChain
 import google.generativeai as genai
 import os
 from langchain_core.messages import HumanMessage
+from langchain_community.document_loaders import PyPDFLoader
 
 load_dotenv(find_dotenv())
 load_dotenv()
 genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
 
 
-app = FastAPI()
+app = FastAPI(root_path="/api/chat")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -31,9 +32,7 @@ class GenerateRequest(BaseModel):
     query: str
 
 @app.post('/generate')
-async def hello(req_body:GenerateRequest):
-    print(req_body.query)
-    print(os.getenv('GOOGLE_API_KEY'))
+async def query(req_body:GenerateRequest):
     llm = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.5,convert_system_message_to_human=True)
     res=llm.invoke(
     [
@@ -43,3 +42,17 @@ async def hello(req_body:GenerateRequest):
     ]
     )
     return {"ai_response":res.content}
+
+async def process_uploaded_file(file: UploadFile):
+  loader = PyPDFLoader(file)
+  pages = loader.load_and_split()
+  print(pages[0])
+
+
+@app.post('/upload')
+async def upload_file(file:UploadFile=File(...)):
+    try:
+        await process_uploaded_file(file)
+    except:
+        return {"error":"There was an error while saving the file"}
+    
