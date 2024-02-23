5e50df47a376da82b80a9a4b34862d95b4d2a8e8
Aditya Bhattad <adityabhattad18@gmail.com>
2024-02-23 23:13:18 +0530

    working chatbot

--- question_answer/src/app.py
+++ question_answer/src/app.py
@@ -2,8 +2,20 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import os
 from dotenv import find_dotenv, load_dotenv
+from pydantic import BaseModel
+from langchain.prompts import PromptTemplate
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_community.vectorstores import FAISS
+from langchain.chains import LLMChain
+import google.generativeai as genai
+import os
+from langchain_core.messages import HumanMessage
 
 load_dotenv(find_dotenv())
+load_dotenv()
+genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
+
 
 app = FastAPI()
 app.add_middleware(
@@ -15,6 +27,19 @@ app.add_middleware(
 )
 
 
-@app.get('/api/chat/')
-def hello():
-    return {"message":"Hello, from k8s cluster"}
+class GenerateRequest(BaseModel):
+    query: str
+
+@app.post('/generate')
+async def hello(req_body:GenerateRequest):
+    print(req_body.query)
+    print(os.getenv('GOOGLE_API_KEY'))
+    llm = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.5,convert_system_message_to_human=True)
+    res=llm.invoke(
+    [
+        HumanMessage(
+            content=req_body.query
+        )
+    ]
+    )
+    return {"ai_response":res.content}
