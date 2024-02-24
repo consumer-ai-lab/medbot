04c9f589c6dbd29c2ca4c0f540092a682b13c12f
Aditya Bhattad <adityabhattad18@gmail.com>
2024-02-24 19:39:43 +0530

    setup query preprocessing service

---
+++ query_preprocessing_service/src/app.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+@app.get("/")
+def root():
+    return {"message":"hello from query_preprocessing_service"}
