8257d1699bd13d7c0b45a25445cb127a80bf7dd9
Aditya Bhattad <adityabhattad18@gmail.com>
2024-02-25 11:43:24 +0530

    unexpected output

--- query_preprocessing_service/src/redis_manager.py
+++ query_preprocessing_service/src/redis_manager.py
@@ -2,7 +2,7 @@ from langchain_community.chat_message_histories import RedisChatMessageHistory
 
 class RedisManager:
     def __init__(self,session_id:str,redis_url="redis://redis-service:6379"):
-        self.history = RedisChatMessageHistory(session_id=session_id,url=redis_url)
+        self.history = RedisChatMessageHistory(session_id=session_id,url=redis_url,ttl=60)
 
     def add_user_message(self,message:str):
         self.history.add_user_message(message=message)
--- question_answer/src/app.py
+++ question_answer/src/app.py
@@ -6,14 +6,17 @@ from pydantic import BaseModel
 from langchain.prompts import PromptTemplate
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.chains import LLMChain
-from langchain.chains import RetrievalQA
+from langchain.chains import ConversationalRetrievalChain,StuffDocumentsChain,LLMChain
 from langchain.vectorstores.pgvector import PGVector
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_community.chat_message_histories import RedisChatMessageHistory
 import google.generativeai as genai
 import os
+
+from langchain.globals import set_debug
+
+set_debug(True)
 
 load_dotenv(find_dotenv())
 genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
@@ -32,15 +35,7 @@ CONNECTION_STRING = os.getenv('CONNECTION_STRING')
 COLLECTION_NAME = os.getenv('CONNECTION_NAME')
 
 def get_custom_prompt():
-   prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", "You're an assistant。"),
-        MessagesPlaceholder(variable_name="history"),
-        ("human", "{question}"),
-    ]
-)
-
-   prompt_template = """
+    prompt_template = """
     As an advanced and reliable medical chatbot, your foremost priority is to furnish the user with precise, evidence-based health insights and guidance. It is of utmost importance that you strictly adhere to the context provided, without introducing assumptions or extrapolations beyond the given information. Your responses must be deeply rooted in verified medical knowledge and practices. Additionally, you are to underscore the necessity for users to seek direct consultation from healthcare professionals for personalized advice.
 
     In crafting your response, it is crucial to:
@@ -53,15 +48,14 @@ def get_custom_prompt():
 
     Your aim is to deliver a response that is not only informative and specific to the user's question but also responsibly framed within the limitations of non-personalized medical advice. Ensure accuracy, clarity, and a strong directive for the user to seek out professional medical evaluation and consultation. Through this approach, you will assist in enhancing the user's health literacy and decision-making capabilities, always within the context provided and without overstepping the boundaries of general medical guidance.
 
-    History of your previous chats with the user: {history}
-    
-    Context: {context}
+    previous message history: {chat_history}
     
     Question: {question}
 
     """
-   prompt = PromptTemplate(template=prompt_template,input_variables=['context','question'])
-   return prompt
+
+    prompt = PromptTemplate(template=prompt_template,input_variables=['question','history'])
+    return prompt
 
 
 def get_retriever():
@@ -77,11 +71,10 @@ def get_retrival_qa_chain():
     prompt = get_custom_prompt()
     retriever = get_retriever()
     llm = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.5,convert_system_message_to_human=True)
-    qa_chain = RetrievalQA.from_chain_type(
+    qa_chain = ConversationalRetrievalChain.from_llm(
         llm=llm,
-        chain_type='stuff',
+        verbose=True,
         retriever=retriever,
-        chain_type_kwargs={'propmt':prompt}
     )
     return qa_chain
 
@@ -93,12 +86,9 @@ def get_chain_with_history():
             session_id=session_id,url="redis://redis-service:6379"
         ),
         input_messages_key="question",
-        history_messages_key="history"
+        history_messages_key="chat_history"
     )
     return chain_with_history
-    
-
-
 
 class GenerateRequest(BaseModel):
     query: str
@@ -109,5 +99,5 @@ def query(req_body:GenerateRequest):
     config = {"configurable": {"session_id": req_body.conversation_id}}
     chain_with_history = get_chain_with_history()
     res = chain_with_history.invoke({"question":req_body.query},config=config)
-    return {"ai_response":res.content}
+    return {"ai_response":res["answer"]}
 
