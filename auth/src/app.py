b97cd5fb32e7954d9de95b8c69c09b9b9f88e362
Aditya Bhattad <adityabhattad18@gmail.com>
2024-03-12 18:37:46 +0530

    setup authentication

---
+++ auth/src/app.py
@@ -0,0 +1,30 @@
+from typing import Annotated
+
+from fastapi import Depends,FastAPI
+from fastapi.security import OAuth2PasswordBearer
+from pydantic import BaseModel
+
+app = FastAPI()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+class User(BaseModel):
+    username:str
+    email:str| None = None
+    full_name: str | None  = None
+    disabled:bool| None = None
+
+
+def fake_decode_token(token):
+    return User(
+        username=token+"fakecoded",email="john@example.com",full_name="John Doe"
+    )
+
+
+async def get_current_user(token:Annotated[str,Depends(oauth2_scheme)]):
+    user = fake_decode_token(token=token)
+    return user
+
+
+async def read_users_me(current_user:Annotated[User,Depends(get_current_user)]):
+    return current_user
