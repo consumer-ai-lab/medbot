d99e7f64c71dd7ddc3c44e95f5b89bf43b6183ef
Aditya Bhattad <adityabhattad18@gmail.com>
2024-04-05 06:02:14 +0530

    add docker config to auth

--- auth/src/database/config.py
+++ auth/src/database/config.py
@@ -1,12 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+DATABASE_CONNECTION_URL = os.getenv("DATABASE_CONNECTION_URL")
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,connect_args={"check_same_thread":False}
+    DATABASE_CONNECTION_URL,connect_args={"check_same_thread":False}
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
