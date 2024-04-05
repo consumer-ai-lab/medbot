b9092a777faae4c7318727574730fb429afd9fe1
Aditya Bhattad <adityabhattad18@gmail.com>
2024-04-05 05:55:08 +0530

    add signout user route

--- auth/src/routes/signout.py
+++ auth/src/routes/signout.py
@@ -1,8 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,status
+from fastapi.responses import JSONResponse
 
 router = APIRouter()
 
 @router.post("/signout")
 async def sign_out_user():
-    return {"route":"signout_user"}
+    response = JSONResponse(content={"message":"User signed out successfully"},status_code=status.HTTP_200_OK)
+    response.delete_cookie("jwt")
+    return response
 
