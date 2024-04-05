83718abc398d374fbe199332c1f0f4089f159da4
Aditya Bhattad <adityabhattad18@gmail.com>
2024-04-05 16:37:35 +0530

    fix typo

--- auth/src/security.py
+++ auth/src/security.py
@@ -17,7 +17,7 @@ load_dotenv()
 
 SECRET_KEY=os.getenv("SECRET_KEY")
 ALGORITHM=os.getenv("ALGORITHM")
-ACCESS_TOKEN_EXPRIRE_MINUTES=os.getenv("ACCESS_TOKEN_EXPRIRE_MINUTES")
+ACCESS_TOKEN_EXPIRE_MINUTES=os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
@@ -52,7 +52,7 @@ def authenticate_user(email:str,password:str,db:Session = Depends(get_db)):
 
 def create_access_token(data:dict):
     to_encode = data.copy()
-    expire = datetime.now(timezone.utc)+timedelta(minutes=int(ACCESS_TOKEN_EXPRIRE_MINUTES))
+    expire = datetime.now(timezone.utc)+timedelta(minutes=int(ACCESS_TOKEN_EXPIRE_MINUTES))
     to_encode.update({"exp":expire})
     encoded_jwt = jwt.encode(to_encode,SECRET_KEY,algorithm=ALGORITHM)
     return encoded_jwt
