c27633946902b1710b14a57a6e5c94683d3ff0cd
adnan-td <generaladnan139@gmail.com>
2024-04-07 22:47:45 +0530

    frontend: static types added

--- frontend/types.d.ts
+++ frontend/types.d.ts
@@ -1,5 +1,49 @@
-type Model = 'Gemini' | 'Chatgpt'
-type Models = Model[]
+declare enum MessageRole {
+  user = 'user',
+  assistant = 'assistant',
+}
 
-type Architechture = 'a' | 'b'
-type Architechtures = Architechture[]
+interface Message {
+  role: MessageRole
+  content: string
+}
+
+interface ChatThread {
+  title: string
+  id: string
+}
+
+declare enum Model {
+  gemini_pro_chat = 'gemini-pro-chat',
+  gemini_pro = 'gemini-pro',
+  llama2 = 'llama2',
+  llama2_uncensored = 'llama2-uncensored',
+}
+
+interface Query {
+  question: string
+  model: Model
+}
+
+interface ApiThreadQuery {
+  user_id: string
+  thread_id: string
+}
+
+interface ApiQuery {
+  user_id: string
+  thread_id: string
+  model: Model
+  question: string
+}
+
+interface QaQuery {
+  model: Model
+  question: string
+  summary: string
+}
+
+interface QaResponse {
+  type: 'OK' | 'REJECTED'
+  response: string
+}
