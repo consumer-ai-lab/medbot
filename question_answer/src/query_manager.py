6b316aaa61ec1f21b9341674e0d49cc6fde5d177
thrombe <yash.n.thombare620@gmail.com>
2024-04-09 21:28:59 +0530

    implement TavilyQaService

--- question_answer/src/query_manager.py
+++ question_answer/src/query_manager.py
@@ -31,6 +31,7 @@ from langchain_community.tools.pubmed.tool import PubmedQueryRun
 
 # from langchain_community.document_loaders import AsyncChromiumLoader
 from langchain_community.document_transformers import BeautifulSoupTransformer
+from langchain.retrievers.tavily_search_api import TavilySearchAPIRetriever, SearchDepth
 from bs4 import BeautifulSoup
 
 from langchain.globals import set_debug
@@ -338,14 +339,11 @@ class PubmedQaService(QaService):
         )
 
     def pubmed_context_chain(self, llm):
-        return (
-            self.genrate_search_queries_chain(llm)
-            | RunnableEach(
-                bound=(
-                    RunnableParallel(
-                        prompt=lambda x: x,
-                        context=lambda x: Document(page_content=PubmedQueryRun().invoke(x)),
-                    )
+        return self.genrate_search_queries_chain(llm) | RunnableEach(
+            bound=(
+                RunnableParallel(
+                    prompt=lambda x: x,
+                    context=lambda x: Document(page_content=PubmedQueryRun().invoke(x)),
                 )
             )
         )
@@ -370,6 +368,52 @@ class PubmedQaService(QaService):
             )
             | printer
         )
+
+
+class TavilyQaService(QaService):
+    def tavily_chain(self):
+        return TavilySearchAPIRetriever(
+            k=1,
+            api_key=os.getenv("TAVILY_AI_API_KEY"),
+            search_depth=SearchDepth.ADVANCED,
+            include_generated_answer=True,
+        )
+
+    def web_context_chain(self, llm, embeddings):
+        return (
+            self.generate_search_queries_chain(llm)
+            | RunnableEach(
+                bound=(
+                    RunnableParallel(
+                        prompt=lambda x: x,
+                        urls=self.tavily_chain(),
+                    )
+                )
+            )
+            | printer
+            | RunnableLambda(lambda x: [d for d in x])
+            | printer
+        )
+
+    def qa_chain(self, llm, embeddings):
+        return (
+            RunnableParallel(
+                prompt=self.question_rephrase_chain(llm),
+                summary=lambda x: x["summary"],
+            )
+            | printer
+            | RunnableParallel(
+                prompt=lambda x: x["prompt"],
+                summary=lambda x: x["summary"],
+                context=self.web_context_chain(llm, embeddings),
+            )
+            | printer
+            | RunnableParallel(
+                context=lambda x: x["context"],
+                response=self.generic_chatbot_prompt_chain(llm),
+            )
+        )
+
 
 class InternetQaService(QaService):
     def __init__(self):
@@ -559,9 +603,26 @@ def get_response(query: QaQuery) -> str:
             strategy = VectorDbQaService()
         case Strategy.pubmed_search:
             strategy = PubmedQaService()
+        case Strategy.web_search_api:
+            strategy = TavilyQaService()
         case _:
             raise RuntimeError("unimplemented strategy")
 
     chain = strategy.qa_chain(llm, embeddings)
     res = chain.invoke(query.dict())
     return res
+
+
+if __name__ == "__main__":
+    query = QaQuery(
+        **{
+            "model": Model.groq_mistral_8x7b,
+            "embeddings_model": EmbeddingsModel.gemini_pro,
+            "strategy": Strategy.web_search_api,
+            "prompt": "what medicines do i take for headache?",
+            "summary": "None",
+        }
+    )
+
+    resp = get_response(query)
+    print(resp)
