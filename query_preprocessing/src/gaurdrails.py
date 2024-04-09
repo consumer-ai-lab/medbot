f65affcb08445d665c3d74a31c9b97a4ced29302
thrombe <yash.n.thombare620@gmail.com>
2024-04-09 23:16:46 +0530

    oof. forgot to extract common from guard rails

--- query_preprocessing/src/gaurdrails.py
+++ query_preprocessing/src/gaurdrails.py
@@ -8,77 +8,10 @@ import pprint
 
 from .create_llm import CreateLLM
 from .types import Model, QaQuery, RelevanceResponse
+from .proompter import printer, hacky_extract_json_dict
+from .proompts import guard_prompt_template, guard_prompt2_template
 
 load_dotenv(find_dotenv())
-
-
-def printer_print(x):
-    print()
-    pprint.pprint(x)
-    print()
-    return x
-
-
-printer = RunnableLambda(printer_print)
-
-# TODO: maybe fuse both these prompt
-guard_prompt = """
-System: you are an AI. your job is to check if the given prompt is related to medical or health related info in any way. summary is only provided for additional context to the prompt and not as the only things you can talk about. DO NOT answer the query. you must output it in json format. {{{{ "related": "NO", "reason": <INSERT REASONING HERE> }}}} or {{{{ "related": "YES", "reason": <INSERT REASONING HERE> }}}}
-
-Summary: user tells AI that they fell on their knee on a playground and hurt themselves.
-Prompt: How do i treat it?
-Output: {{{{ "related": "YES", "reason": "I am allowed to tell you how to treat their knee." }}}}
-
-Summary: user tells AI they they have cough.
-Prompt: How do i complete the first level of mario?
-Output: {{{{ "related": "NO", "reason": "I am not allowed to tell you about Mario. it is not related to any medical context." }}}}
-
-Summary: None
-Prompt: How do i eat a cabbage?
-Output: {{{{ "related": "NO", "reason": "I cannot tell you anything about eating cabbage. not related to any medical context" }}}}
-
-Summary: user is suffering from back pain.
-Prompt: what medicines should i take for cough?
-Output: {{{{ "related": "YES", "reason": "I am allowed to tell you which medicines you should take for cough even though it is not related to back pain." }}}}
-
-Summary: {context}
-Prompt: {prompt}
-Output: """
-guard_prompt_template = PromptTemplate(
-    template=guard_prompt, input_variables=["context", "prompt"]
-)
-
-guard_prompt2 = """
-You are an AI which is built to classify whether a given query is related to medical in any sense or not. Do not answer the query,
-just classify it as 'YES', 'NO', 'MAYBE', or 'ILLEGAL'. If you think the given query is related to medical then answer will be 'YES'.
-If you think the given query is not related to medical then output 'NO'. If you are are 70 percent sure that this can be related to medical
-by any sense like whether it requires any medical treatment or doctor supervision then output 'MAYBE' else your answer.
-You must output an JSON consisting of 2 parameters:- 1) related 2) reason. Output this :- {{{{"related":"YES", "reason":"<Insert-Your-Reason-Here>"}}}} or {{{{"related":"NO","reason":"<Insert-Your-Reason-Here">}}}}
-The value of related will be either yes or no or maybe. The value of reason will the reason that will be inferred by you that why do you think
-that it's answer is yes, no or maybe. You should have a valid reason to judge the query. donot hallucinate. if you cannot infer anything just output 'MAYBE'.
-I will give you an example for the current situation.
-
-1. first situation:-
-Prompt: I hurt myself while playing football
-AI response: {{{{"related":"YES", "reason": "User wants to know how to treat a wound they got while playing"}}}}
-
-2. second situation:-
-Prompt: I want to know best restaurant near me.
-AI response: {{{{"related":"NO", "reason": "User is asking for restaurant, it is not related to medical field by any context"}}}}
-
-If user is asking you about wrong practices which are related to medical field but they illegal, then output 'ILLEGAL'.
-Wrong practices includes planning a murder of someone, asking about torturing someone using medical toolkits, something like this.
-
-3. third situation:-
-Prompt: I want to take revenge with someone who always bully me in school. I want to set a trap for him like jigsaw and torture him with medical toolkits
-AI response: {{{{"related":"ILLEGAL", "reason": "User is asking for wrong practices. I donot support helping or promoting such actions"}}}}
-
-Prompt: {prompt}
-AI response:
-"""
-guard_prompt2_template = PromptTemplate(
-    template=guard_prompt2, input_variables=["prompt"]
-)
 
 
 def guard_chain1(llm):
