4cb753a8e04681191be58f59b1208e1a5e345459
Prathamesh314 <kurvep18@gmail.com>
2024-04-18 02:50:12 +0530

    Integrated rate limiting

--- query_preprocessing/src/routes/chat.py
+++ query_preprocessing/src/routes/chat.py
@@ -15,6 +15,7 @@ from ..redis_manager import get_redis_manager
 from ..chat_summary_manager import SummaryProompter
 from ..create_llm import CreateLLM
 import asyncio
+from ..tokenbucket import get_token_bucket
 
 router = APIRouter()
 
@@ -23,23 +24,36 @@ async def generator(resp):
         yield f"{chunk} "
         await asyncio.sleep(0.1)
 
-
+token_bucket = None
 @router.post("/generate")
 async def get_ai_message(
     query: ApiQuery, current_user: UserBase = Depends(get_current_user)
 ):
-    llm = CreateLLM(query.model, query.embeddings_model).getModel()
-
-    chat_manager = get_redis_manager(current_user.user_id)
-    if not chat_manager.has_thread(query.thread_id):
-        chat_manager.add_thread(ChatThread(id=query.thread_id, title=query.prompt))
-    chat_history = chat_manager.get_chat(query.thread_id)
-
-    chat_manager.add_message(
-        query.thread_id, Message(role=MessageRole.user, content=query.prompt)
-    )
-
     try:
+        global token_bucket
+        print(f"{current_user=}")
+        token_bucket = get_token_bucket(key=current_user.user_id, sizeof_bucket=11) if token_bucket is None else token_bucket
+        if token_bucket.size() == 1 and token_bucket.exists():
+            raise HTTPException(
+                status_code=429,
+                detail="Too many API requests. Please wait for some time.",
+            )
+        elif not token_bucket.exists():
+            token_bucket = get_token_bucket(key=current_user.user_id, sizeof_bucket=11)
+        
+        print(f"{token_bucket.size()=}")
+        token_bucket.consume()
+        llm = CreateLLM(query.model, query.embeddings_model).getModel()
+
+        chat_manager = get_redis_manager(current_user.user_id)
+        if not chat_manager.has_thread(query.thread_id):
+            chat_manager.add_thread(ChatThread(id=query.thread_id, title=query.prompt))
+        chat_history = chat_manager.get_chat(query.thread_id)
+
+        chat_manager.add_message(
+            query.thread_id, Message(role=MessageRole.user, content=query.prompt)
+        )
+
         summarizer = SummaryProompter()
         summary = summarizer.get_summary(llm, chat_history)
 
